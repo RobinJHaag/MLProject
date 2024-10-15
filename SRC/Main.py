0237880a30e561083c01225e258a2d43349f28db
Robin Jannes Haag <160750833+RobinJHaag@users.noreply.github.com>
2024-10-15 17:20:52 +0200

    Comments Cleanup
    Cleaned up Comments & added minor changes to simulation.

--- SRC/Main.py
+++ SRC/Main.py
@@ -1,28 +1,20 @@
-# python
 from DataSimulator import DataSimulator
 from PredictMed import train_and_evaluate_models
 from Plotter import Plotter
 
 
 def main():
-    # Initialize the simulator with a specific random state for reproducibility
     simulator = DataSimulator(random_state=42)
-
-    # Simulate for 36 months
     df = simulator.simulate_sales_and_stock(months_to_simulate=36)
 
-    # Display the simulation results
     print("Gesamte Simulationsergebnisse:\n")
     print(df)
 
-    # Plot the shortage status over time
     plotter = Plotter(df)
     plotter.plot_shortage_status()
 
-    # Train and evaluate models
     y_test, y_pred_linear, y_pred_rf = train_and_evaluate_models(df)
 
-    # Display the predictions
     print("\nActual vs Predicted (Linear Regression):")
     for actual, pred in zip(y_test, y_pred_linear):
         print(f"Actual: {actual}, Predicted: {pred}")
@@ -30,6 +22,7 @@ def main():
     print("\nActual vs Predicted (Random Forest Regression):")
     for actual, pred in zip(y_test, y_pred_rf):
         print(f"Actual: {actual}, Predicted: {pred}")
+
 
 if __name__ == "__main__":
     main()
